897dcc9a6010e194040462acc1609dd55bf5f19b
wzr26 <quangdung123.qad@gmail.com>
2026-05-26 06:17:51 +0000

    Minor fixes

--- algorithms/binarysearch.cpp
+++ algorithms/binarysearch.cpp
@@ -11,23 +11,23 @@ int main(){
         cin >> arr[i];
     }
     for (int j = 0; j < n - 1; j++){
-    if (arr[j + 1] < arr[j]){
-        cout << "This is not a sorted array.\n";
-        return 0;
+        if (arr[j + 1] < arr[j]){
+            cout << "This is not a sorted array.\n";
+            return 0;
     }
 }
     cout << "Enter a number to find: ";
     cin >> num;
-int low = 0, high = n - 1;
-int middle = (low + high)/2;
-while (num != arr[middle]){
-    if(num > arr[middle]){
-        low = middle + 1;
-        middle = (low + high)/2;
+    int low = 0, high = n - 1;
+    int middle = (low + high)/2;
+    while (num != arr[middle]){
+        if(num > arr[middle]){
+            low = middle + 1;
+            middle = (low + high)/2;
     }
-    else if(num < arr[middle]){
-        high = middle - 1;
-        middle = (low + high)/2;
+        else if(num < arr[middle]){
+            high = middle - 1;
+            middle = (low + high)/2;
     }
 }
 cout << "The number " << num << " is at index " << middle + 1 << ".\n";
