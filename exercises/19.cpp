1413a4b735b57b88acaebd7ffaee6898cd3db462
wzr26 <quangdung123.qad@gmail.com>
2026-06-03 15:48:39 +0000

    Struct

---
+++ exercises/19.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Student {
+    int id;
+    string name;
+    int age;
+};
+
+int main() {
+
+    vector<Student> students;
+
+    Student s;
+
+    cout << "ID: ";
+    cin >> s.id;
+
+    cin.ignore();
+    
+    cout << "Name: ";
+    getline(cin, s.name);
+
+    cout << "Age: ";
+    cin >> s.age;
+
+    students.push_back(s);
+
+    cout << "\n--- Student List ---\n";
+
+    for (int i = 0; i < students.size(); i++) {
+        cout << "ID: " << students[i].id << "\n";
+        cout << "Name: " << students[i].name << "\n";
+        cout << "Age: " << students[i].age << "\n";
+    }
+
+    return 0;
+}
